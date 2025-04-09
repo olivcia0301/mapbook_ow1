fc66f03ba7d6531f2affc579de821dd68df4e6d2
student <walczynskaoliwia@gmail.com>
2025-04-09 10:58:15 +0200

    wip: Add Menu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils.model import users
 from utils.controller import get_user_info
 
 def main():
-    print(f"Witaj {users[0]["name"]}")
+    print(f"Witaj {users[0]['name']}")
 
     while True:
         print("============MENU==============")
